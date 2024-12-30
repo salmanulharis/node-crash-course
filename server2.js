40eca96dac8cd05477afe0553afc3aa9bd853424
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-30 15:18:57 +0530

    API using conventional http module method

--- server2.js
+++ server2.js
@@ -1,3 +1,4 @@
+import { match } from 'assert';
 import { createServer } from 'http';
 const PORT = process.env.PORT;
 
@@ -12,7 +13,21 @@ const server = createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
-    }else {
+    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        if(user){
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(user));
+            res.end(); 
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({ message: 'User not found' }));
+            res.end();
+        }
+        
+    } else {
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
         res.write(JSON.stringify({ message: 'Route not found' }));

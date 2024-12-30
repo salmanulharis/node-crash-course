9a44cc1f457c2c5eab3cff5d6c62487f12c221e6
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-30 12:49:27 +0530

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello wrold</h1>');
+    try {
+        if (req.method === 'GET'){
+            if(req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>Home Page</h1>');
+            } else if(req.url === '/about'){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>About Page</h1>');
+            }else {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server Error');
+
+    }
 })
 
 server.listen(PORT, () => {
